260a9c166099e4c159fa827c1ad437cd2c09e196
tery <vladislavutkin92@gmail.com>
2019-06-27 16:15:14 +0300

    решение задач

--- Extended-Syntax/task.js
+++ Extended-Syntax/task.js
@@ -1,4 +1,4 @@
-
+'use strict';
 
 function calculateQuadraticEquation(){
     let a = +window.a.value;
@@ -10,9 +10,41 @@ function calculateQuadraticEquation(){
     span.textContent = "х = "+result;
 }
 
-function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    //return x;
+function getResult(a, b, c) { // 2x^2+4x-3=0.
+    if(a == 0 || b == 0 || c == 0) alert('Введите числа не равные "0"');
+    else {
+        let result = (Math.pow(b, 2)) - (4 * a * c),// b^2 - 4ac;
+            arrResult = [];
+        console.log(result, arrResult);
+        if (result === 0) arrResult.push(result);
+        else if (result < 0) arrResult.push('нет корня');
+        else if(result > 0) {
+            arrResult[0] = Math.round((-b + Math.sqrt(result)) / (2 * a));
+            arrResult[1] = Math.round((-b - Math.sqrt(result)) / (2 * a));
+        }// x = -b +- Math.sqrt(D) / 2 * a
+        return arrResult;
+    }
+}
+
+function calculateAverageRating(){
+    let marks = window.marks.value.split("").map(Number).filter((n)=> !isNaN(n) && n > 0);
+    let averageMark = getAverageMark(marks);
+    window.averageMark.textContent = averageMark;
+}
+
+function getAverageMark(marks){
+    let value = 0;
+    function getSearch(a) {
+        for (let i = 0; i < a; i++) {
+            value += marks[i];
+            }
+    } // просто чтобы не повторяться
+    if (marks.length <= 5) getSearch(marks.length);   
+    else if (marks.length > 5) {
+        getSearch(5); 
+        console.log(marks.slice(0,5));
+    }
+    return (marks.length <= 5) ? Math.round(value / marks.length) : Math.round(value / 5);
 }
 
 function calculateDrinkTask(){
@@ -23,18 +55,7 @@ function calculateDrinkTask(){
 }
 
 function askDrink(name,dateOfBirthday){
-    // код для задачи №2 писать здесь
-    //console.log(result)
-    //return result;
-}
-
-function calculateAverageRating(){
-    let marks = window.marks.value.split("").map(Number).filter((n)=> !isNaN(n) && n > 0);
-    let averageMark = getAverageMark(marks);
-    window.averageMark.textContent = averageMark;
-}
-
-function getAverageMark(marks){
-    // код для задачи №3 писать здесь
-    //return averageMark;
+    let date = ((Math.floor(Date.now() / 1000) - Math.floor(dateOfBirthday / 1000)) * 1000) / 31536000000; //вычисление целых лет
+    if (date < 18) return `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;
+    else return `Не желаете ли олд-фэшн, ${name}?`;
 }
