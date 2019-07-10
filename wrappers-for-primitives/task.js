51dc9f18fcf4b56738eb4a5b7dc1b359af8d23cb
TomuraCreator <vladislavutkin92@gmail.com>
2019-07-10 20:04:14 +0300

    решена первая задача

--- wrappers-for-primitives/task.js
+++ wrappers-for-primitives/task.js
@@ -1,7 +1,8 @@
+'use strict';
 function calculateMortgage() {
-    let percent = window.percent.value;
-    let contribution = window.contribution.value;
-    let amount = window.amount.value;
+    let percent = +window.percent.value;
+    let contribution = +window.contribution.value;
+    let amount = +window.amount.value;
     let date = window.date.value;
 
     let result = calculateTotalMortgage(percent, contribution, amount, date);
@@ -9,11 +10,20 @@ function calculateMortgage() {
     span.textContent = result;
 }
 
-function calculateTotalMortgage(percent, contribution, amount, date) {
-
-    // код для задачи №1 писать здесь
-    //return totalAmount;
-}
+function calculateTotalMortgage(percent = 0, contribution = 0, amount = 0, date) {
+    let mounth = (Date.parse(date) - Date.now()) / 2592000000;
+    if(typeof percent === 'number' || typeof contribution === 'number' || typeof amount === 'number') {
+        let S = amount - contribution;
+        let P = Math.round(percent / 12);
+        let totalAmount = S * (P + P / (((1 + P) ^ mounth) - 1)); // S*(P+P/(((1+P)^n)-1))
+        console.log(totalAmount, mounth, S, P);
+        return totalAmount.toFixed(2);
+    } else {
+        console.log('Нихера');
+    }
+    
+    
+}   
 
 function sayHello() {
     let name = window.personName.value;
