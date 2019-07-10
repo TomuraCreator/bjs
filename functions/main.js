ee65759249b8bbe96bd1d54085b224d111384419
TomuraCreator <vladislavutkin92@gmail.com>
2019-07-10 15:05:43 +0300

    Решена третья задача

--- functions/main.js
+++ functions/main.js
@@ -29,3 +29,60 @@ function showSolutionsMessage(a, b, c) {
 showSolutionsMessage(4, 4, 4);
 showSolutionsMessage(1, -5, 6);
 showSolutionsMessage(16, -8, 1);
+
+function getAverageScore(data) { 
+    
+}
+
+function getArrayAverage() {
+    
+    // let count = 0;
+    // for(let r in d) {
+    //     console.log(typeof(r), typeof(d));
+    //     // for ( let i = 0; i < d.r.length; i++) {
+    //     //     count += d.algebra[i];
+    //     // }
+    // }
+    return this.algebra.length;
+    // return count / d.algebra.length;
+
+    
+}
+console.log( getAverageScore({
+    algebra : [2, 4, 5, 2, 5, 4],
+    geometry : [2, 4, 5],
+    russian : [3, 3, 4, 5],
+    physics : [5, 5],
+    music : [ 2, 2, 5],
+    english : [4, 4, 3],
+    poetry : [5, 3, 4],
+    chemistry : [2],
+    french : [4, 4] 
+}));
+
+function getPersonData(secretData) {
+    return getObject(secretData);
+}
+function getObject(base) {
+    let fName = (base.aaa == 0) ? 'Родриго' : 'Эмильо',
+        lName = (base.bbb == 0) ? 'Родриго' : 'Эмильо';
+    return { firstName: fName, lastName: lName }; 
+}
+
+
+console.log(getPersonData({
+    aaa: 0, 
+    bbb: 0  
+}))
+console.log(getPersonData({
+    aaa: 1, 
+    bbb: 0  
+}))
+console.log(getPersonData({
+    aaa: 1, 
+    bbb: 1  
+}))
+console.log(getPersonData({
+    aaa: 0, 
+    bbb: 1  
+}))
