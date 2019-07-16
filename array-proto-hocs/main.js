aa5d31e51aee483df9085e20be8e12ee166070a6
tery <vladislavutkin92@gmail.com>
2019-07-16 22:51:10 +0300

    частичное решение

--- array-proto-hocs/main.js
+++ array-proto-hocs/main.js
@@ -19,3 +19,39 @@ compareArrays([8, 9, 5, 4], [8, 9, 5, 4, 8, 3, 5]); // false, разные зн�
 compareArrays([9, 2, 4, 8, 2], [9, 2, 4]); // false, разные значения
 compareArrays([1, 2, 3], [2, 3, 1]); // false, разные индексы, хотя и одинаковые значения
 compareArrays([8, 1, 2], [8, 1, 2]); // true
+
+const sum = (t) => {
+    let sums = 0;
+    for(let i of t) {
+        sums += i;
+    }
+    return sums;
+}
+
+function memoize(fun, limit) {
+    let memory = [];
+    if (memory.length > limit) {
+        memory = memory.splice(1, limit - 1);
+    }
+
+    console.log(memory)
+    return function() {
+        memory.push({
+            args: Array.from(arguments),
+            result: fun(arguments)
+        })
+    }
+    
+}
+
+const mSum = memoize(sum, 5);
+console.log(mSum(5, 6, 2));
+console.log(mSum(6, 6));
+console.log(mSum(3, 6));
+console.log(mSum(5, 6, 2));
+console.log(mSum(6, 6));
+console.log(mSum(3, 6));
+console.log(mSum(5, 6, 2));
+console.log(mSum(6, 6));
+console.log(mSum(3, 6));
+
