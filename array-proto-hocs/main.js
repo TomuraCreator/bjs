779f4adf6f3e4ad1f546c2a0320b734aa35c50f5
tery <vladislavutkin92@gmail.com>
2019-07-20 02:32:53 +0300

    задача выполнена

--- array-proto-hocs/main.js
+++ array-proto-hocs/main.js
@@ -1,9 +1,10 @@
 function compareArrays(arr1, arr2) {
+    count.reset();
     const manyAray = (arr1 >= arr2) ? arr1 : arr2,
         lessArray = (arr2 <= arr1) ? arr2 : arr1;
     const back = manyAray.every(element => element === lessArray[count.getNext()]);
-    console.log(back);
-    count.reset();
+
+    return back;
 }
 function makeCounter() {
     var currentCount = 0;
@@ -14,11 +15,10 @@ function makeCounter() {
 };
 const count = makeCounter();
 
-compareArrays([8, 9], [8, 9]); //true
-compareArrays([8, 9, 5, 4], [8, 9, 5, 4, 8, 3, 5]); // false, разные значения
-compareArrays([9, 2, 4, 8, 2], [9, 2, 4]); // false, разные значения
-compareArrays([1, 2, 3], [2, 3, 1]); // false, разные индексы, хотя и одинаковые значения
-compareArrays([8, 1, 2], [8, 1, 2]); // true
+function checkArray(a, b) {
+   return a.find(element => compareArrays(element.args, b))
+}
+    
 
 const sum = (t) => {
     let sums = 0;
@@ -26,32 +26,46 @@ const sum = (t) => {
         sums += i;
     }
     return sums;
-}
+} // сумма для массивоподобного объекта arguments
 
 function memoize(fun, limit) {
     let memory = [];
-    if (memory.length > limit) {
-        memory = memory.splice(1, limit - 1);
-    }
-
     console.log(memory)
     return function() {
-        memory.push({
-            args: Array.from(arguments),
-            result: fun(arguments)
-        })
+        if(checkArray(memory, Array.from(arguments))) {
+            const funcVar = checkArray(memory, Array.from(arguments));
+            console.info(`Прошлый результат с числами ${funcVar.args}: ${funcVar.result}`)
+            memory.push({
+                args: funcVar.args,
+                result: funcVar.result,
+                info: 'Из памяти'
+            })
+        } else {
+            memory.push({
+                args: Array.from(arguments),
+                result: fun(arguments)
+            })
+            console.log(fun(arguments));
+        }
+        
+        if (memory.length > limit) memory.shift();
+        
     }
-    
 }
-
 const mSum = memoize(sum, 5);
-console.log(mSum(5, 6, 2));
-console.log(mSum(6, 6));
-console.log(mSum(3, 6));
-console.log(mSum(5, 6, 2));
-console.log(mSum(6, 6));
-console.log(mSum(3, 6));
-console.log(mSum(5, 6, 2));
-console.log(mSum(6, 6));
-console.log(mSum(3, 6));
+
+
+mSum(5, 6, 2);
+mSum(6, 6, 1, 5);
+mSum(3, 6, 6);
+mSum(6, 6, 3, 3);
+mSum(3, 6);
+mSum(5, 6, 2, 2);
+mSum(6, 6);
+mSum(6, 6);
+mSum(6, 6, 3);
+
+
+
+
 
